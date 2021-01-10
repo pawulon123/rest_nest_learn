19cd1a1f8af9411daecc2f382828bf478ce5afe2
pawelchodzba123 <pawul0n@op.pl>
2021-01-11 00:23:20 +0100

    route note

---
+++ src/note/note.entity.spec.ts
@@ -0,0 +1,7 @@
+// import { NotesEntity.Ts } from './note.entity.ts';
+
+// describe('NotesEntity.Ts', () => {
+//   it('should be defined', () => {
+//     expect(new NotesEntity.Ts()).toBeDefined();
+//   });
+// });
