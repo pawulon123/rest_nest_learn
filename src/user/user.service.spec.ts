792c23fc90989d04adf411ce0b589ae8aec9aa56
pawelchodzba123 <pawul0n@op.pl>
2021-01-18 00:01:25 +0100

    setup getRepositoryToken in  user.Service.spec

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,12 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { users } from './user.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('UserService', () => {
   let service: UserService;
 
+  
+  let mockRepository= {
+    index:jest.fn(),
+    findOne:jest.fn(),
+    create:jest.fn(),
+    updata:jest.fn(),
+    destroy:jest.fn()
+   
+  }
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide:getRepositoryToken(users),
+          useValue: mockRepository
+        },
+      
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
